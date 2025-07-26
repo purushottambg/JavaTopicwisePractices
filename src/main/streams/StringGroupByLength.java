e3240cab8f2245859f92e125eab0572109063daa
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-07-26 11:25:35 +0530

    Added the stream problem

--- src/main/streams/StringGroupByLength.java
+++ src/main/streams/StringGroupByLength.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.LogManager;
+import java.util.stream.Collectors;
+
 public class StringGroupByLength {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.stream("Hum tum itne pass aarhe hai , kabhi dur na jaaye".split(" ")).toList();
+
+        Map<Integer, List<String>> stringMap = strings.stream().collect(
+                Collectors.groupingBy(String::length)
+        );
+
+        for (Map.Entry<Integer, List<String>> obj: stringMap.entrySet() ){
+            System.out.println(obj.getKey()+"->"+obj.getValue());
+        }
+    }
 }
